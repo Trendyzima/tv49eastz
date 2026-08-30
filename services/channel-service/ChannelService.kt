9c62638352e9c040c5958bed90ecf514408300a8
Trendyzima <momanyitrendy@gmail.com>
2026-08-31 01:18:38 +0300

    Align ChannelService with canonical packages

--- services/channel-service/ChannelService.kt
+++ services/channel-service/ChannelService.kt
@@ -1,7 +1,7 @@
-package tv49eastz.services.channelservice
+package com.tv49eastz.services.channelservice
 
-import tv49eastz.core.model.Channel
-import tv49eastz.integration.contracts.ChannelProvider
+import com.tv49eastz.core.model.Channel
+import com.tv49eastz.integration.contracts.ChannelProvider
 
 class ChannelService(private val providers: List<ChannelProvider>) {
     suspend fun listChannels(): List<Channel> = providers.flatMap { it.listChannels() }.distinctBy(Channel::id)
