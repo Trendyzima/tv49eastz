a674cddf440529aa63534209e72353d2ea1b39ed
Trendyzima <momanyitrendy@gmail.com>
2026-08-31 01:18:43 +0300

    Align PlaylistService with canonical packages

--- services/playlist-service/PlaylistService.kt
+++ services/playlist-service/PlaylistService.kt
@@ -1,7 +1,7 @@
-package tv49eastz.services.playlistservice
+package com.tv49eastz.services.playlistservice
 
-import tv49eastz.core.model.Playlist
-import tv49eastz.integration.contracts.PlaylistProvider
+import com.tv49eastz.core.model.Playlist
+import com.tv49eastz.integration.contracts.PlaylistProvider
 
 class PlaylistService(private val providers: List<PlaylistProvider>) {
     suspend fun loadPlaylists(): List<Playlist> = providers.flatMap { it.loadPlaylists() }.distinctBy(Playlist::id)
