418833470da907e5e39226267a1ed2378c888da5
Trendyzima <momanyitrendy@gmail.com>
2026-08-31 12:41:06 +0300

    test(mtls): enforce certificate-to-device identity binding

---
+++ stream-gateway/mtls_identity_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"math/big"
+	"testing"
+	"time"
+)
+
+func testCertificate(t *testing.T) *x509.Certificate {
+	t.Helper()
+	key, err := rsa.GenerateKey(rand.Reader, 2048); if err != nil { t.Fatal(err) }
+	now := time.Now()
+	tpl := &x509.Certificate{SerialNumber: big.NewInt(1), Subject: pkix.Name{CommonName: "device-a"}, NotBefore: now.Add(-time.Minute), NotAfter: now.Add(time.Hour), KeyUsage: x509.KeyUsageDigitalSignature, ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth}}
+	der, err := x509.CreateCertificate(rand.Reader, tpl, tpl, &key.PublicKey, key); if err != nil { t.Fatal(err) }
+	cert, err := x509.ParseCertificate(der); if err != nil { t.Fatal(err) }; return cert
+}
+
+func TestDeviceIdentityFromTLSUsesCertificateFingerprint(t *testing.T) {
+	cert := testCertificate(t)
+	r := NewDeviceRegistry()
+	if err := r.Register(DeviceRecord{DeviceID:"device-a", PrincipalID:"p", Fingerprint:certificateFingerprint(cert), Channels:map[string]bool{"camera":true}, Enabled:true}); err != nil { t.Fatal(err) }
+	d, err := DeviceIdentityFromTLS(&tls.ConnectionState{PeerCertificates:[]*x509.Certificate{cert}}, r)
+	if err != nil || d.DeviceID != "device-a" { t.Fatalf("identity binding failed: %#v %v", d, err) }
+}
+
+func TestDeviceIdentityFromTLSRejectsUnregisteredCertificate(t *testing.T) {
+	cert := testCertificate(t); r := NewDeviceRegistry()
+	if _, err := DeviceIdentityFromTLS(&tls.ConnectionState{PeerCertificates:[]*x509.Certificate{cert}}, r); err == nil { t.Fatal("accepted unregistered certificate") }
+}
+
+func TestNewMutualTLSConfigRequiresClientCA(t *testing.T) {
+	if _, err := NewMutualTLSConfig(tls.Certificate{Certificate:[][]byte{{1}}}, nil); err == nil { t.Fatal("accepted missing client CA") }
+}
