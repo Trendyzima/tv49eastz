fc8e62143b18efa717d6bca3ecec9bb3ce3aeed5
Trendyzima <momanyitrendy@gmail.com>
2026-09-02 01:11:02 +0300

    Add IPTV reel metadata for richer feed cards

--- tv-receiver/src/main/java/com/fadcam/tv/IptvReel.java
+++ tv-receiver/src/main/java/com/fadcam/tv/IptvReel.java
@@ -9,9 +9,18 @@ public final class IptvReel {
     public final String referrer;
     public final String userAgent;
     public final String source;
+    public final String logoUrl;
+    public final String country;
+    public final String category;
 
     public IptvReel(String id, String channel, String title, String url, String quality,
                     String referrer, String userAgent, String source) {
+        this(id, channel, title, url, quality, referrer, userAgent, source, "", "", "");
+    }
+
+    public IptvReel(String id, String channel, String title, String url, String quality,
+                    String referrer, String userAgent, String source, String logoUrl,
+                    String country, String category) {
         this.id = id;
         this.channel = channel;
         this.title = title;
@@ -20,5 +29,8 @@ public final class IptvReel {
         this.referrer = referrer;
         this.userAgent = userAgent;
         this.source = source;
+        this.logoUrl = logoUrl;
+        this.country = country;
+        this.category = category;
     }
 }
