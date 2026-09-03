0f6aa24206c17846985ba4706b83e6c7c2743086
Trendyzima <momanyitrendy@gmail.com>
2026-09-03 09:27:18 +0300

    fix(permissions): restore settings API import

--- app/src/permissionSafe/java/com/fadcam/ui/OnboardingPermissionsFragment.java
+++ app/src/permissionSafe/java/com/fadcam/ui/OnboardingPermissionsFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
